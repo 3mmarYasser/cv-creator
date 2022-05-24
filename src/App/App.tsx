b71674d828092732eaa979f34b82706231209ada
Ammar <entar99ammar@gmail.com>
2022-05-24 20:16:00 +0200

    initial Files

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import {BrowserRouter as Router , Routes , Route} from "react-router-dom";
 import HomeScreen from "../screens/HomeScreen/HomeScreen";
 import Navbar from "../components/Navbar/Navbar";
-import "./App.scss"
+import "./App.scss";
 const App:React.FC = ()=> {
   return (
       <Router>
           <div className="container">
               <Navbar/>
-              <Routes>
-                  <Route index element={<HomeScreen/>} />
-                  <Route path="*" element={<div>404 Page</div>} />
-              </Routes>
+              <div className="">
+                  <Routes>
+                      <Route index element={<HomeScreen/>} />
+                      <Route path="*" element={<div>404 Page</div>} />
+                  </Routes>
+              </div>
           </div>
 
       </Router>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,32 +1,92 @@
-import React,{useState} from 'react';
-import {NavLink} from "react-router-dom";
+import React, {useEffect, useState ,useRef} from 'react';
 import classNames from "classnames";
+import {gsap} from "gsap";
+import {NavLink} from "react-router-dom";
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import TranslateIcon from '@mui/icons-material/Translate';
+import ContactSupportIcon from '@mui/icons-material/ContactSupport';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+import TryIcon from '@mui/icons-material/Try';
+
 import Styles from './Navbar.module.scss';
 
 const Navbar :React.FC = () => {
-    const [menu, setMenu] = useState(false);
+    const [menu, setMenu] = useState<boolean>(false);
+    const menuRef = useRef<HTMLDivElement | null>(null);
+
+    useEffect(() :void=>{
+       window.addEventListener("resize",() :void=>{
+           if( menu && window.innerWidth > 1000) {
+               setMenu(false)
+           }
+       })
+       menu ? document.body.classList.add("overlay"): document.body.classList.remove("overlay");
+       if(menu){
+           gsap.fromTo(menuRef.current , {scale : "0"} ,{scale:"1" })
+       }
+   },[menu]);
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLLIElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    const closeMenu = () :void=>{
+        if(window.innerWidth <= 1000){
+            gsap.fromTo(menuRef.current , {scale : "1"} ,{scale:"0"})
+            setTimeout(()=>{
+                gsap.to(menuRef.current , {scale:1 ,duration:0})
+                setMenu(false);
+            },500)
+        }
+    }
+
     return (
-        <header className={classNames("flex justify-between h-[60px]  p-[10px] w-[100vw]" , Styles.Header)}>
-            <div className="flex justify-center">
-                <svg className="pr-[20px]"  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 148 137">
+        <header className={classNames("bg_color t-0 l-0 flex justify-between h-[60px] fixed  p-[10px] w-[100vw] z-[98]" , [Styles.Header])}>
+            <div className="flex justify-center items-center">
+                <svg className={classNames("w-[60px] h-[60px] pr-[20px] z-[110]",Styles.logo)}  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 148 137">
                     <title>Logo</title>
                     <rect className="fls_1" x="31" width="117" height="108" rx="12"/>
                     <rect className="fls_2" x="16" y="17" width="113" height="105" rx="12"/>
                     <rect className="fls_3" y="29" width="117" height="108" rx="12"/>
                 </svg>
-                <h1 className="logo_font flex justify-center self-center text-4xl font-bold ">CV Builder <span>.</span></h1>
+                <h1 className="logo_font flex justify-center self-center text-3xl font-bold">CV Builder <span>.</span></h1>
             </div>
-            <div className={classNames([[Styles.header_content],{[Styles.menu] : menu},{"back_drop" : menu}])}>
-                <ul className={classNames("flex")}>
-                    <li><NavLink to="/templates">Templates</NavLink></li>
-                    <li><NavLink to="/contactus">Contact Us</NavLink></li>
-                    <li><NavLink to="/ar">Arabic</NavLink></li>
-                </ul>
-                <button>Give It a Try</button>
-            </div>
-            <MenuIcon className={classNames("init_icon cursor-pointer", [Styles.menuIcon])} onClick={()=>{setMenu(!menu)}}/>
 
+            <div ref={menuRef} className={classNames("flex",[[Styles.header_content],{[Styles.menu] : menu},{"back_drop" : menu}])}>
+                <ul  className={classNames("flex")}>
+                    <li  onMouseOver={handleClick}  className={classNames("flex items-center",[Styles.drop_able_menu])}>Templates <ExpandMoreIcon className="main_color w-[15px] h-[15px]"/> </li>
+                    <li onClick={closeMenu}><NavLink to="/contactus">Contact Us <ContactSupportIcon  className="main_color"/></NavLink></li>
+                    <li onClick={closeMenu}><NavLink to="/ar">Arabic <TranslateIcon className="main_color"/></NavLink></li>
+                </ul>
+                <button onClick={closeMenu}  className="main_button">Give It a Try <TryIcon className="btn_icon"/></button>
+                {menu ? <button className={classNames("absolute top-[5px] right-[5px] p-[2px] rounded-full",[Styles.closeMenu])} onClick={closeMenu}><CloseIcon className={Styles.closeIcon}/></button>:null}
+            </div>
+
+            <MenuIcon className={classNames("init_icon cursor-pointer self-center", [Styles.menuIcon])} onClick={()=>{setMenu(!menu)}}/>
+
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}
+            >
+                <MenuItem
+                    className="bg-amber-200"
+                    onClick={()=>{handleClose();closeMenu();}}>
+                    <NavLink to="templates/new">New Templates</NavLink>
+                </MenuItem>
+                <MenuItem   onClick={()=>{handleClose();closeMenu();}}>All Templates</MenuItem>
+            </Menu>
         </header>
     );
 };
