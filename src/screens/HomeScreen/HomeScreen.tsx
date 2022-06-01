8d6cb4d30405522f235ba4142eae26138a06536f
Kareem-3del <kareem.adel.zayed@gmail.com>
2022-06-01 20:53:14 +0200

    Home-Page animation

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import MainSlide from "./components/MainSlide/MainSlide";
 import AppSlide from "./components/AppSlide/AppSlide";
-const HomeScreen :React.FC = () => {
+import TemplatesSlide from "./components/TemplatesSlide/TemplatesSlide";
+
+const HomeScreen: React.FC = () => {
     return (
         <div>
-        <MainSlide></MainSlide>
-        <AppSlide/>
+            <MainSlide></MainSlide>
+            <AppSlide/>
+            <TemplatesSlide/>
         </div>
     );
 };
 
-export default HomeScreen ;
+export default HomeScreen;
