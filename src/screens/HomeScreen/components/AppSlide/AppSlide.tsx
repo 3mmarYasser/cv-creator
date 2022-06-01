f7fa14c4c1cc86e7555038010cdeb61341249280
Kareem-3del <kareem.adel.zayed@gmail.com>
2022-06-01 19:51:59 +0200

    Home-Page animation

--- src/screens/HomeScreen/components/AppSlide/AppSlide.tsx
+++ src/screens/HomeScreen/components/AppSlide/AppSlide.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 const AppSlide :React.FC = () => {
     return (
         <div className="flex flex-col mx-auto my-5 w-screen">
-            <div className="container mx-auto flex justify-center items-center space-x-5">
+            <div className="container mx-auto  justify-center items-center space-x-5 hidden sm:flex">
              <Link className="px-5 flex flex-col justify-center items-center border-b-4 border-ghost hover:border-primary hover:text-primary transition duration-300" to={"#"}>
                  <b className="text-xl">Resume Builder</b>
                  <span className="my-1 font-light">Make a stand-out resume</span>
@@ -19,21 +19,22 @@ const AppSlide :React.FC = () => {
                     <span className="my-1 font-light">Make a stand-out resume</span>
                 </Link>
 
+
             </div>
 
-            <div className="container mx-auto flex justify-center items-center py-10 space-x-10 ">
-                <div className="w-1/2">
-                    <img className="w-full" src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt=""/>
+            <div className="container mx-auto flex flex-col sm:flex-row justify-center items-center py-10">
+                <div className="p-8">
+                    <img  width="600px" src="https://enhancv.com/static/72b8926bde7ba27c488e369a217b32b6/db955/enhancv-resume-builder%402x.png" alt=""/>
                 </div>
 
-                    <div className="w-1/2 max-w-xl">
-                        <h1 className="text-4xl mb-5">
+                    <div className="max-w-xl my-10 w-full mx-auto sm:mx-1  p-10 sm:p-0 pt-0">
+                        <h1 className="text-4xl mb-5 lg:text-6xl">
                             The resume builder trusted by professionals
                         </h1>
-                        <p className="text-neutral">
+                        <p className="opacity-75">
                             Create a visually stunning resume that makes recruiters want to read it and explore it. Our resume creator will guide you through the process. Focus on the content, while we take care of the formatting.
                         </p>
-                        <button className="mt-5 btn btn-lg btn-primary text-primary-content">
+                        <button className="mt-5 btn btn-md btn-primary text-primary-content">
                             Build my resume now
                         </button>
                     </div>
