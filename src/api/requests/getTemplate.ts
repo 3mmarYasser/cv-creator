07950f9d9f09ef0a2017441aabddcb526784be0c
Ammar <entar99ammar@gmail.com>
2022-06-02 15:51:25 +0200

    Outer Html Recognition And Read + Simple Edit

--- src/api/requests/getTemplate.ts
+++ src/api/requests/getTemplate.ts
@@ -1,7 +1,7 @@
 import Axios from "../config/Axios";
 const getTemplate = async ():Promise<any>=>{
     try {
-      return await Axios.get("/templates/templateone.htm0l")
+      return await Axios.get("/templates/templateone.html")
     }catch (e:any){
         throw new Error(e)
     }
