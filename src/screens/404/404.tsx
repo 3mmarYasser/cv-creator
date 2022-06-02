ef0196c722242021088d1f5e9024065e37158ba1
Kareem-3del <kareem.adel.zayed@gmail.com>
2022-06-02 04:58:46 +0200

    Navbar.tsx Fix

--- src/screens/404/404.tsx
+++ src/screens/404/404.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Page404: React.FC = () => {
     return (
         <div className="w-screen h-screen flex justify-center items-center text-6xl bg-base-300">
-           <b className="animate-bounce flex flex-col items-center justify-center "><h1 className="text-8xl">404</h1> Not Found</b>
+           <b className="animate-bounce flex flex-col items-center justify-center "><h1 className="text-8xl">404</h1> Not found</b>
         </div>
     );
 };
