d1ff0bbaa162e4eef3be560e9a5cb1fa3008da81
Kareem-3del <kareem.adel.zayed@gmail.com>
2022-06-02 04:51:25 +0200

    Navbar.tsx Fix

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,6 +4,7 @@ import HomeScreen from "../screens/HomeScreen/HomeScreen";
 import Navbar from "../components/Navbar/Navbar";
 import "./App.scss";
 import Page404 from "../screens/404/404";
+import Editor from "../components/Editor/Index/Editor";
 const App:React.FC = ()=> {
   return (
       <Router>
@@ -12,7 +13,9 @@ const App:React.FC = ()=> {
               <div className="">
                   <Routes>
                       <Route index element={<HomeScreen/>} />
+                      <Route path="editor" element={<Editor/>} />
                       <Route path="*" element={<Page404/>} />
+
                   </Routes>
               </div>
           </div>
