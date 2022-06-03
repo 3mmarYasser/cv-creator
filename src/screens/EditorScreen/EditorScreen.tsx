21ed237be30b3d2a2cf2311fd06f0138284527a0
Ammar <entar99ammar@gmail.com>
2022-06-03 15:22:43 +0200

    Editor

--- src/screens/EditorScreen/EditorScreen.tsx
+++ src/screens/EditorScreen/EditorScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import {Template} from "./EditorScreen.service";
 import EditRender from "./components/EditRender/EditRender";
 import Styles from './EditorScreen.module.scss'
@@ -13,12 +13,17 @@ const EditorScreen :React.FC<Props> = () => {
         else if(err) return <p className="text-red-500">{err}</p>
         return <EditRender data={data} />
     }
+    const editorRef = useRef <HTMLDivElement |null>(null);
     return (
         <div className="pt-[100px]">
-            <div className=" content_color pt-[40px] w-[100vw]">
+            <div className="flex flex-col content_color pt-[40px] w-[100vw]">
+                <button className="self-center ">Download</button>
                 <section className="flex justify-center">
-                    {render()}
+                    <div ref={editorRef}>
+                        {render()}
+                    </div>
                 </section>
+
             </div>
         </div>
     );
