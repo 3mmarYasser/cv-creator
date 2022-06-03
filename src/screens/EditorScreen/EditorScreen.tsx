73e40e7ab593d04ec00c4f4b4029a63341a22fe5
Ammar <entar99ammar@gmail.com>
2022-06-03 16:32:11 +0200

    Save PDF

--- src/screens/EditorScreen/EditorScreen.tsx
+++ src/screens/EditorScreen/EditorScreen.tsx
@@ -1,12 +1,16 @@
 import React,{useRef} from 'react';
+import ReactDOMServer from "react-dom/server";
 import {Template} from "./EditorScreen.service";
 import EditRender from "./components/EditRender/EditRender";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 import Styles from './EditorScreen.module.scss'
 
 interface Props{
 
 }
 const EditorScreen :React.FC<Props> = () => {
+
     const {data , loading ,err} = Template();
     const render = ():JSX.Element=>{
         if(loading)return <p>Loading</p>
@@ -14,10 +18,28 @@ const EditorScreen :React.FC<Props> = () => {
         return <EditRender data={data} />
     }
     const editorRef = useRef <HTMLDivElement |null>(null);
+
+    const DownloadPDF = ()=>{
+        const input:any = editorRef.current;
+        html2canvas(input).then(canvas => {
+            const imgData = canvas.toDataURL("image/svg");
+            
+            const pdf:any = new jsPDF({
+                orientation: "portrait",
+                unit: "px",
+                format: [1330, 940]
+            });
+            pdf.addImage(imgData, "svg", 0, 0);
+            pdf.save("cv.pdf");
+        });
+
+    }
     return (
         <div className="pt-[100px]">
-            <div className="flex flex-col content_color pt-[40px] w-[100vw]">
-                <button className="self-center ">Download</button>
+            <div className="content_color pt-[40px] w-[100vw]">
+                <section className="w-[100vw] justify-center flex mb-5">
+                    <button  onClick={DownloadPDF} className="self-center btn btn-primary text-primary-content px-10">Download</button>
+                </section>
                 <section className="flex justify-center">
                     <div ref={editorRef}>
                         {render()}
