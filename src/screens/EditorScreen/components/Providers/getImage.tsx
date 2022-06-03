b2306bf068bbed3de8b95b7cb4dc0dddcb044f30
Ammar <entar99ammar@gmail.com>
2022-06-03 22:02:17 +0200

    Editor

--- src/screens/EditorScreen/components/Providers/getImage.tsx
+++ src/screens/EditorScreen/components/Providers/getImage.tsx
@@ -1,7 +1,7 @@
 import html2canvas from "html2canvas";
 const getImageFromRef =async (ref:HTMLElement|any):Promise<string> =>{
     let ImageRes:string = "";
-  await  html2canvas(ref).then(canvas =>  {
+  await  html2canvas(ref,{scale:2}).then(canvas =>  {
         ImageRes =  canvas.toDataURL("image/png");
     });
     return ImageRes
