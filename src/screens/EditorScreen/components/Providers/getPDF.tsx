5f26db2c63732fba55be2fd91a320c063f9c075c
Ammar <entar99ammar@gmail.com>
2022-06-03 21:01:22 +0200

    Editor

--- src/screens/EditorScreen/components/Providers/getPDF.tsx
+++ src/screens/EditorScreen/components/Providers/getPDF.tsx
@@ -1,11 +1,14 @@
 import JsPDF from "jspdf";
 import {getImageFromRef} from "./getImage";
 const DownloadPDFByRef = async (ref:HTMLElement|null):Promise<void> =>{
-        const pdf:any = new JsPDF();
+        const pdf:any = new JsPDF({
+                unit: 'px',
+                format: [1000, 510],
+
+        });
         const Image = await getImageFromRef(ref);
         pdf.addImage(Image, "JPEG", 0, 0);
         pdf.save(`cv.pdf`);
 }
-export {
-        DownloadPDFByRef
-}
+
+export {DownloadPDFByRef}
