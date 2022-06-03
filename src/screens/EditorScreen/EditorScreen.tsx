329c1d4fc9d051f6e1707dd039e4875eb27ae971
Ammar <entar99ammar@gmail.com>
2022-06-03 17:12:08 +0200

    PDF

--- src/screens/EditorScreen/EditorScreen.tsx
+++ src/screens/EditorScreen/EditorScreen.tsx
@@ -1,4 +1,4 @@
-import React,{useRef} from 'react';
+import React,{useRef , useState} from 'react';
 import ReactDOMServer from "react-dom/server";
 import {Template} from "./EditorScreen.service";
 import EditRender from "./components/EditRender/EditRender";
@@ -18,11 +18,10 @@ const EditorScreen :React.FC<Props> = () => {
         return <EditRender data={data} />
     }
     const editorRef = useRef <HTMLDivElement |null>(null);
+    const [image , setImage]=useState<string>("")
 
-    const DownloadPDF = ()=>{
+    const DownloadPDF = ():void =>{
         const input:any = editorRef.current;
-        html2canvas(input).then(canvas => {
-            const imgData = canvas.toDataURL("image/png");
         //     const pdf:any = new jsPDF({
         //         orientation: "landscape",
         //         unit: "px"
@@ -30,20 +29,24 @@ const EditorScreen :React.FC<Props> = () => {
         //     pdf.addImage(imgData, "JPEG", 0, 0);
         //     pdf.save("cv.pdf");
         // });
-
-    }
+        html2canvas(input).then(canvas => {
+            const imgData = canvas.toDataURL("image/png");
+            setImage(imgData)
+        });
+         }
     return (
         <div className="pt-[100px]">
             <div className="content_color pt-[40px] w-[100vw]">
                 <section className="w-[100vw] justify-center flex mb-5">
                     <button  onClick={DownloadPDF} className="self-center btn btn-primary text-primary-content px-10">Download</button>
                 </section>
-                <section className="flex justify-center">
-                    <div ref={editorRef}>
+                <section className="flex flex-col items-center justify-center">
+                    <div className="w-[930px] h-[1330px]" ref={editorRef}>
                         {render()}
                     </div>
-                </section>
+                    <img src={image} alt="image"/>
 
+                </section>
             </div>
         </div>
     );
