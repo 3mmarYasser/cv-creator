2c08085c951144f642a78a995c121e107e9f31af
Ammar <entar99ammar@gmail.com>
2022-06-04 10:44:51 +0200

    Editor

--- src/screens/EditorScreen/EditorScreen.tsx
+++ src/screens/EditorScreen/EditorScreen.tsx
@@ -1,38 +1,46 @@
-import React,{useRef , useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {Template} from "./EditorScreen.service";
 import EditRender from "./components/EditRender/EditRender";
 import {DownloadPDFByRef} from "./components/Providers/getPDF";
 import {removeSelection} from "./components/Providers/Selection";
 import {getImageFromRef} from "./components/Providers/getImage";
 import StyledModal from "../../components/StyledModal/StyledModal";
+import {getElByID} from "./components/Providers/getInHTML";
+
 // import Styles from './EditorScreen.module.scss'
 
-interface Props{
+interface Props {
 
 }
-const EditorScreen :React.FC<Props> = () => {
 
-    const {data , loading ,err} = Template();
-    const render = ():JSX.Element=>{
-        if(loading)return <p>Loading</p>
-        else if(err) return <p className="text-red-500">{err}</p>
-        return <EditRender data={data} />
+const EditorScreen: React.FC<Props> = () => {
+
+    const {data, loading, err} = Template();
+    const render = (): JSX.Element => {
+        if (loading) return <p>Loading</p>
+        else if (err) return <p className="text-red-500">{err}</p>
+        return <EditRender data={data}/>
     }
-    const editorRef = useRef <HTMLDivElement |null>(null);
-    const [image , setImage]=useState<string>()
-    const DownloadCV = async ():Promise<void>=>{
-        const ChickIsEditing:HTMLDivElement|null = document.querySelector('[data-render-page]');
-        if(ChickIsEditing?.matches("[editor]")){
+
+    const editorRef = useRef<HTMLDivElement | null>(null);
+    const [image, setImage] = useState<string>()
+    const DownloadCV = async (): Promise<void> => {
+
+        const ChickIsEditing: HTMLDivElement | null = document.querySelector('[data-render-page]');
+        if (ChickIsEditing?.matches("[editor]")) {
             removeSelection(ChickIsEditing)
+            getElByID("header-hover-bar")?.classList.remove("hidden");
         }
         await DownloadPDFByRef(editorRef.current)
     }
-    const PreviewCV = async ():Promise<void>=>{
-        const ChickIsEditing:HTMLDivElement|null = document.querySelector('[data-render-page]');
-        if(ChickIsEditing?.matches("[editor]")){
+
+    const PreviewCV = async (): Promise<void> => {
+        const ChickIsEditing: HTMLDivElement | null = document.querySelector('[data-render-page]');
+        if (ChickIsEditing?.matches("[editor]")) {
             removeSelection(ChickIsEditing)
+            getElByID("header-hover-bar")?.classList.remove("hidden");
         }
-        setImage(await  getImageFromRef(editorRef.current))
+        setImage(await getImageFromRef(editorRef.current))
 
     }
 
@@ -41,17 +49,21 @@ const EditorScreen :React.FC<Props> = () => {
             <div className="bg-base-200 pt-[40px] w-[100vw]">
 
                 <section className="w-[100vw] justify-center flex mb-5">
-                    <button onClick={DownloadCV}   className="self-center btn btn-primary text-primary-content px-10 ">Download</button>
-                    <label  htmlFor="PreviewCV" onClick={PreviewCV} className="self-center btn btn-main text-primary-content px-10 ml-[20px] modal-open">Preview</label>
+                    <button onClick={DownloadCV}
+                            className="self-center btn btn-primary text-primary-content px-10 ">Download
+                    </button>
+                    <label htmlFor="PreviewCV" onClick={PreviewCV}
+                           className="self-center btn btn-main text-primary-content px-10 ml-[20px] modal-open">Preview</label>
                 </section>
                 <section className="flex flex-col items-center justify-center">
-                   <div className="shadow">
-                       <div ref={editorRef} >
-                           {render()}
-                       </div>
-                   </div>
+                    <div className="shadow">
+                        <div ref={editorRef}>
+                            {render()}
+                        </div>
+                    </div>
                     <StyledModal id="PreviewCV">
-                        <img draggable={false} onContextMenu={(e)=>e.preventDefault()} contextMenu="false"  src={image}  alt={"Show PdF"}/>
+                        <img draggable={false} onContextMenu={(e) => e.preventDefault()} contextMenu="false" src={image}
+                             alt={"Show PdF"}/>
                     </StyledModal>
                 </section>
             </div>
@@ -60,4 +72,4 @@ const EditorScreen :React.FC<Props> = () => {
     );
 };
 
-export default EditorScreen ;
+export default EditorScreen;
--- src/screens/EditorScreen/components/Sections/ResumeHeader/ResumeHeaderHover.tsx
+++ src/screens/EditorScreen/components/Sections/ResumeHeader/ResumeHeaderHover.tsx
@@ -1,51 +1,56 @@
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 import StyledSwitch from "../../../../../components/StyledSwitch/StyledSwitch";
-import {getElByID} from "../../Providers/getInHTML";
+import {getElByID, getElementByAttr} from "../../Providers/getInHTML";
 
-interface Styling{
-    title:boolean
-    phone:boolean
-    link:boolean
-    email:boolean
-    location:boolean
-    uppercase:boolean
-    photo:boolean
-    curve:boolean
+interface Styling {
+    title: boolean
+    phone: boolean
+    link: boolean
+    email: boolean
+    location: boolean
+    uppercase: boolean
+    photo: boolean
+    curve: boolean
 }
+
 interface Props {
-    top:number
-    left:number
+    top: number
+    left: number
 }
-const ResumeHeaderHover :React.FC<Props> = ({top , left}) => {
-    const HTitle     =  getElByID("header-title"),
-          HPhone     =  getElByID("h-phone"),
-          HLink      =  getElByID("h-link"),
-          HEmail     =  getElByID("h-email"),
-          HLocation  =  getElByID("h-location"),
-          HUppercase =  getElByID("header-name"),
-          HPhoto     =  getElByID("h-phone"),
-          HCurve     =  getElByID("image-curve");
 
-    const [styling ,SetStyling ] = useState<Styling>({
+const ResumeHeaderHover: React.FC<Props> = ({top, left}) => {
+    const HTitle = getElByID("header-title"),
+        HPhone = getElByID("h-phone"),
+        HLink = getElByID("h-link"),
+        HEmail = getElByID("h-email"),
+        HLocation = getElByID("h-location"),
+        HUppercase = getElByID("header-name"),
+        HPhoto = getElByID("h-photo"),
+        HCurve = getElementByAttr("[data-h-image]")
+
+    const [styling, SetStyling] = useState<Styling>({
         title: !HTitle?.classList.contains("hidden"),
         phone: !HPhone?.classList.contains("hidden"),
-        link:true,
-        email:true,
-        location:true,
-        uppercase:true,
-        photo:true,
-        curve:true
+        link: !HLink?.classList.contains("hidden"),
+        email: !HEmail?.classList.contains("hidden"),
+        location: !HLocation?.classList.contains("hidden"),
+        uppercase: HUppercase ? HUppercase?.classList.contains("uppercase") : false,
+        photo: !HPhoto?.classList.contains("hidden"),
+        curve: HCurve ? HCurve?.classList.contains("image-curve") : false
     })
     console.log(styling)
     return (
-        <div id="header-hover-bar" className={`hidden absolute flex justify-center items-center p-[10px] h-[40px] bg_color rounded-[20px]`}  style={{top:`${(top - 40)}px`,left:`${left}px`}}>
+        <div id="header-hover-bar"
+             className={`hidden absolute flex justify-center items-center p-[10px] h-[40px] bg_color rounded-[20px]`}
+             style={{top: `${(top - 40)}px`, left: `${left}px`}}>
             <div className="dropdown">
-                <label tabIndex={0} className="cursor-pointer flex"> <i className="material-icons cursor-pointer">settings</i></label>
-                <ul  tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                <label tabIndex={0} className="cursor-pointer flex"> <i
+                    className="material-icons cursor-pointer">settings</i></label>
+                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
 
                     <li>
-                        <StyledSwitch check={styling.title} toggle={(e:any)=>{
-                            SetStyling({...styling , title:!e.target.checked})
+                        <StyledSwitch check={styling.title} toggle={(e: any) => {
+                            SetStyling({...styling, title: !e.target.checked})
                             HTitle?.classList.toggle("hidden")
                         }}>
                             Show title
@@ -53,13 +58,68 @@ const ResumeHeaderHover :React.FC<Props> = ({top , left}) => {
                     </li>
 
                     <li>
-                        <StyledSwitch check={styling.phone} toggle={(e:any)=>{
-                            SetStyling({...styling , phone:!e.target.checked});
+                        <StyledSwitch check={styling.phone} toggle={(e: any) => {
+                            SetStyling({...styling, phone: !e.target.checked});
                             HPhone?.classList.toggle("hidden");
                         }}>
                             Show Phone
                         </StyledSwitch>
                     </li>
+
+                    <li>
+                        <StyledSwitch check={styling.link} toggle={(e: any) => {
+                            SetStyling({...styling, link: !e.target.checked});
+                            HLink?.classList.toggle("hidden");
+                        }}>
+                            Show Link
+                        </StyledSwitch>
+                    </li>
+
+                    <li>
+                        <StyledSwitch check={styling.email} toggle={(e: any) => {
+                            SetStyling({...styling, email: !e.target.checked});
+                            HEmail?.classList.toggle("hidden");
+                        }}>
+                            Show Email
+                        </StyledSwitch>
+                    </li>
+
+                    <li>
+                        <StyledSwitch check={styling.location} toggle={(e: any) => {
+                            SetStyling({...styling, location: !e.target.checked});
+                            HLocation?.classList.toggle("hidden");
+                        }}>
+                            Show Location
+                        </StyledSwitch>
+                    </li>
+                    <li>
+                        <StyledSwitch check={styling.photo} toggle={(e: any) => {
+                            SetStyling({...styling, photo: !e.target.checked});
+                            HPhoto?.classList.toggle("hidden");
+                        }}>
+                            Show Photo
+                        </StyledSwitch>
+                    </li>
+                    <hr/>
+                    <li>
+                        <StyledSwitch check={styling.uppercase} toggle={(e: any) => {
+                            SetStyling({...styling, uppercase: !e.target.checked});
+                            HUppercase?.classList.toggle("uppercase");
+                        }}>
+                            Uppercase Name
+                        </StyledSwitch>
+                    </li>
+                    
+
+                    <li>
+                        <StyledSwitch check={styling.curve} toggle={(e: any) => {
+                            SetStyling({...styling, curve: !e.target.checked});
+                            HCurve?.classList.toggle("image-curve");
+                        }}>
+                            Curve Image
+                        </StyledSwitch>
+                    </li>
+
 
                 </ul>
             </div>
@@ -67,4 +127,4 @@ const ResumeHeaderHover :React.FC<Props> = ({top , left}) => {
     );
 };
 
-export default ResumeHeaderHover ;
+export default ResumeHeaderHover;
