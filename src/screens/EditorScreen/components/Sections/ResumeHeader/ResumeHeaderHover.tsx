0009f2557542169be9bbae5b406eda40c843a169
Ammar <entar99ammar@gmail.com>
2022-06-04 11:19:08 +0200

    Editor

--- src/screens/EditorScreen/components/Sections/ResumeHeader/ResumeHeaderHover.tsx
+++ src/screens/EditorScreen/components/Sections/ResumeHeader/ResumeHeaderHover.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import StyledSwitch from "../../../../../components/StyledSwitch/StyledSwitch";
 import {getElByID, getElementByAttr} from "../../Providers/getInHTML";
+import StyledModal from "../../../../../components/StyledModal/StyledModal";
 
 interface Styling {
     title: boolean
@@ -38,92 +39,115 @@ const ResumeHeaderHover: React.FC<Props> = ({top, left}) => {
         photo: !HPhoto?.classList.contains("hidden"),
         curve: HCurve ? HCurve?.classList.contains("image-curve") : false
     })
-    console.log(styling)
+    const switchContainer = (
+        <>
+            <li>
+                <StyledSwitch check={styling.title} toggle={(e: any) => {
+                    SetStyling({...styling, title: !e.target.checked})
+                    HTitle?.classList.toggle("hidden")
+                }}>
+                    Show title
+                </StyledSwitch>
+            </li>
+
+            <li>
+                <StyledSwitch check={styling.phone} toggle={(e: any) => {
+                    SetStyling({...styling, phone: !e.target.checked});
+                    HPhone?.classList.toggle("hidden");
+                }}>
+                    Show Phone
+                </StyledSwitch>
+            </li>
+
+            <li>
+                <StyledSwitch check={styling.link} toggle={(e: any) => {
+                    SetStyling({...styling, link: !e.target.checked});
+                    HLink?.classList.toggle("hidden");
+                }}>
+                    Show Link
+                </StyledSwitch>
+            </li>
+
+            <li>
+                <StyledSwitch check={styling.email} toggle={(e: any) => {
+                    SetStyling({...styling, email: !e.target.checked});
+                    HEmail?.classList.toggle("hidden");
+                }}>
+                    Show Email
+                </StyledSwitch>
+            </li>
+
+            <li>
+                <StyledSwitch check={styling.location} toggle={(e: any) => {
+                    SetStyling({...styling, location: !e.target.checked});
+                    HLocation?.classList.toggle("hidden");
+                }}>
+                    Show Location
+                </StyledSwitch>
+            </li>
+
+            <li>
+                <StyledSwitch check={styling.photo} toggle={(e: any) => {
+                    SetStyling({...styling, photo: !e.target.checked});
+                    HPhoto?.classList.toggle("hidden");
+                }}>
+                    Show Photo
+                </StyledSwitch>
+            </li>
+
+            <hr/>
+
+            <li>
+                <StyledSwitch check={styling.uppercase} toggle={(e: any) => {
+                    SetStyling({...styling, uppercase: !e.target.checked});
+                    HUppercase?.classList.toggle("uppercase");
+                }}>
+                    Uppercase Name
+                </StyledSwitch>
+            </li>
+
+            <li>
+                <StyledSwitch check={styling.curve} toggle={(e: any) => {
+                    SetStyling({...styling, curve: !e.target.checked});
+                    HCurve?.classList.toggle("image-curve");
+                }}>
+                    Curve Image
+                </StyledSwitch>
+            </li>
+        </>
+    )
     return (
-        <div id="header-hover-bar"
-             className={`hidden absolute flex justify-center items-center p-[10px] h-[40px] bg_color rounded-[20px]`}
-             style={{top: `${(top - 40)}px`, left: `${left}px`}}>
-            <div className="dropdown">
-                <label tabIndex={0} className="cursor-pointer flex"> <i
-                    className="material-icons cursor-pointer">settings</i></label>
-                <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+        <>
+            <div id="header-hover-bar"
+                 className={`hidden absolute flex justify-center items-center p-[10px] h-[40px] bg_color rounded-[20px]`}
+                 style={{top: `${(top - 40)}px`, left: `${left}px`, transform: "translate(-50% , 0)"}}>
 
-                    <li>
-                        <StyledSwitch check={styling.title} toggle={(e: any) => {
-                            SetStyling({...styling, title: !e.target.checked})
-                            HTitle?.classList.toggle("hidden")
-                        }}>
-                            Show title
-                        </StyledSwitch>
-                    </li>
+                <div className="dropdown dropdown-right setting-header-hover">
+                    <label tabIndex={0} className="cursor-pointer flex"> <i
+                        className="material-icons cursor-pointer">settings</i></label>
+                    <ul tabIndex={0}
+                        className="list-none dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                        {switchContainer}
+                    </ul>
+                </div>
 
-                    <li>
-                        <StyledSwitch check={styling.phone} toggle={(e: any) => {
-                            SetStyling({...styling, phone: !e.target.checked});
-                            HPhone?.classList.toggle("hidden");
-                        }}>
-                            Show Phone
-                        </StyledSwitch>
-                    </li>
+                <div style={{display: "none"}} className="setting-header-hover-phone">
+                    <label htmlFor="resume-header-modal" className="cursor-pointer flex">
+                        <i className="material-icons cursor-pointer">settings</i>
+                    </label>
 
-                    <li>
-                        <StyledSwitch check={styling.link} toggle={(e: any) => {
-                            SetStyling({...styling, link: !e.target.checked});
-                            HLink?.classList.toggle("hidden");
-                        }}>
-                            Show Link
-                        </StyledSwitch>
-                    </li>
-
-                    <li>
-                        <StyledSwitch check={styling.email} toggle={(e: any) => {
-                            SetStyling({...styling, email: !e.target.checked});
-                            HEmail?.classList.toggle("hidden");
-                        }}>
-                            Show Email
-                        </StyledSwitch>
-                    </li>
-
-                    <li>
-                        <StyledSwitch check={styling.location} toggle={(e: any) => {
-                            SetStyling({...styling, location: !e.target.checked});
-                            HLocation?.classList.toggle("hidden");
-                        }}>
-                            Show Location
-                        </StyledSwitch>
-                    </li>
-                    <li>
-                        <StyledSwitch check={styling.photo} toggle={(e: any) => {
-                            SetStyling({...styling, photo: !e.target.checked});
-                            HPhoto?.classList.toggle("hidden");
-                        }}>
-                            Show Photo
-                        </StyledSwitch>
-                    </li>
-                    <hr/>
-                    <li>
-                        <StyledSwitch check={styling.uppercase} toggle={(e: any) => {
-                            SetStyling({...styling, uppercase: !e.target.checked});
-                            HUppercase?.classList.toggle("uppercase");
-                        }}>
-                            Uppercase Name
-                        </StyledSwitch>
-                    </li>
-                    
-
-                    <li>
-                        <StyledSwitch check={styling.curve} toggle={(e: any) => {
-                            SetStyling({...styling, curve: !e.target.checked});
-                            HCurve?.classList.toggle("image-curve");
-                        }}>
-                            Curve Image
-                        </StyledSwitch>
-                    </li>
-
-
-                </ul>
+                </div>
             </div>
-        </div>
+
+            <input type="checkbox" id="resume-header-modal" className="modal-toggle"/>
+            <label htmlFor="resume-header-modal" className="modal cursor-pointer modal-bottom">
+                <label className="modal-box relative" htmlFor="">
+                    <ul>
+                        {switchContainer}
+                    </ul>
+                </label>
+            </label>
+        </>
     );
 };
 
