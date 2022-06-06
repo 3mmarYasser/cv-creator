106c2026f740267481cc0714ed7971d7f46d5c82
Ammar <entar99ammar@gmail.com>
2022-06-06 02:39:38 +0200

    s

--- src/screens/DashboardScreen/components/DashIndex/DashIndex.tsx
+++ src/screens/DashboardScreen/components/DashIndex/DashIndex.tsx
@@ -64,33 +64,7 @@ const DashIndex: React.FC<Props> = () => {
         <div className="w-[100vw] min-h-[100vh] pt-[170px]">
             <div
                 className="w-[100vw]  m-auto flex flex-col justify-between md:flex-row ">
-                <div className="w-[100vw] lg:w-[70%]">
-                    <div className="hero bg-base-200">
-                        <div className="hero-content flex-col lg:flex-row-reverse">
-                            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                                <div className="card-body">
-                                    <div className="form-control">
-                                        <label className="label">
-                                            <span className="label-text">Email</span>
-                                        </label>
-                                        <input type="text" placeholder="email" className="input input-bordered"/>
-                                    </div>
-                                    <div className="form-control">
-                                        <label className="label">
-                                            <span className="label-text">Password</span>
-                                        </label>
-                                        <input type="text" placeholder="password" className="input input-bordered"/>
-                                        <label className="label">
-                                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                        </label>
-                                    </div>
-                                    <div className="form-control mt-6">
-                                        <button className="btn btn-primary">Login</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div className="w-[100vw] bg-base-200 lg:w-[70%]">
 
                 </div>
                 <div className="flex flex-col justify-between w-[100vw] lg:w-[30%]">
