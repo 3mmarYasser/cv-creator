c073daef1777ab46c43d68287e5f6bf3db076b68
Ammar <entar99ammar@gmail.com>
2022-06-06 02:18:35 +0200

    Dash

--- src/screens/DashboardScreen/components/DashIndex/DashIndex.tsx
+++ src/screens/DashboardScreen/components/DashIndex/DashIndex.tsx
@@ -10,79 +10,48 @@ import {
 const data = [
     {
         name: 'May',
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
+        Views: 4000,
     },
     {
         name: 'Jun',
-        uv: 3000,
-        pv: 1398,
-        amt: 2210,
+        Views: 3000,
+
     },
     {
         name: 'May',
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
+        Views: 2000,
+
     },
     {
         name: 'Jun',
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
+        Views: 2780,
+
     },
     {
         name: 'May',
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
+        Views: 1890,
+
     },
     {
-        name: '1Years Ago',
-        uv: 2390,
-        pv: 3800,
-        amt: 2500,
+        name: '1Year',
+        Views: 2390,
+
     }
 ];
 
 const data2 = [
     {
-        name: 'Page A',
-        uv: 590,
-        pv: 800,
-        amt: 1400,
+        name: 'May',
+        Resume: 590,
     },
     {
-        name: 'Page B',
-        uv: 868,
-        pv: 967,
-        amt: 1506,
+        name: 'Jan',
+        Resume: 868,
     },
     {
-        name: 'Page C',
-        uv: 1397,
-        pv: 1098,
-        amt: 989,
-    },
-    {
-        name: 'Page D',
-        uv: 1480,
-        pv: 1200,
-        amt: 1228,
-    },
-    {
-        name: 'Page E',
-        uv: 1520,
-        pv: 1108,
-        amt: 1100,
-    },
-    {
-        name: 'Page F',
-        uv: 1400,
-        pv: 680,
-        amt: 1700,
-    },
+        name: '1Year',
+        Resume: 1397,
+    }
 ];
 
 interface Props {
@@ -93,30 +62,58 @@ const DashIndex: React.FC<Props> = () => {
 
     return (
         <div
-            className="w-[100vw] grid pt-[170px] grid-cols-1 gap-y-12 gap-6 md:grid-cols-2 lg:grid-cols-4 bg-base-300 min-h-[100vh]">
+            className="w-[100vw] lg:w-[1200px] grid pt-[170px] grid-cols-1  gap-2 md:grid-cols-2 bg-base-300 min-h-[100vh]">
 
-            <div className=" p-[10px] w-[100vw] md:w-[400px] lg:w-[600px]">
-                <ResponsiveContainer width="100%" height={200}>
-                    <AreaChart
-                        width={500}
-                        height={200}
-                        data={data}
-                        syncId="anyId"
-                        margin={{
-                            top: 10,
-                            right: 30,
-                            left: 0,
-                            bottom: 0,
-                        }}
-                    >
-                        <CartesianGrid strokeDasharray="3 3"/>
-                        <XAxis dataKey="name"/>
-                        <YAxis/>
-                        <Tooltip/>
-                        <Area type="monotone" dataKey="pv" stroke="#82ca9d" fill="#82ca9d"/>
-                    </AreaChart>
-                </ResponsiveContainer>
-                <h1 className="text-center">SITE Views</h1>
+            <div className="flex flex-col justify-between w-[100vw] md:w-[50vw]">
+                <div className=" p-[10px]">
+                    <ResponsiveContainer width="100%" height={200}>
+                        <AreaChart
+                            width={500}
+                            height={200}
+                            data={data}
+                            syncId="anyId"
+                            margin={{
+                                top: 10,
+                                right: 30,
+                                left: 0,
+                                bottom: 0,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3"/>
+                            <XAxis dataKey="name"/>
+                            <YAxis/>
+                            <Tooltip/>
+                            <Area type="monotone" dataKey="Views" stroke="#82ca9d" fill="#82ca9d"/>
+                        </AreaChart>
+                    </ResponsiveContainer>
+                    <h1 className="text-center">SITE Views</h1>
+                </div>
+                <div className=" p-[10px] ">
+                    <ResponsiveContainer width="100%" height={400}>
+                        <ComposedChart
+                            data={data2}
+                            margin={{
+                                top: 20,
+                                right: 80,
+                                bottom: 20,
+                                left: 20,
+                            }}
+                        >
+                            <CartesianGrid stroke="#f5f5f5"/>
+                            <XAxis dataKey="name"
+                                   label={{value: 'Date', position: 'insideBottomRight', offset: 0}}
+                                   scale="band"/>
+                            <YAxis label={{value: 'Resume Created', angle: -90, position: 'insideLeft'}}/>
+                            <Tooltip/>
+                            <Legend/>
+                            <Area type="monotone" dataKey="Resume" fill="#8884d8"
+                                  stroke="#8884d8"/>
+                        </ComposedChart>
+                    </ResponsiveContainer>
+                </div>
+            </div>
+            <div className="w-[50vw]">
+
             </div>
 
         </div>
