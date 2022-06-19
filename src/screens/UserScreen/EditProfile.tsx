de9d3c467d12652da08805774fdee60eabb3ec4e
Ammar <entar99ammar@gmail.com>
2022-06-19 22:57:08 +0200

    FIX

--- src/screens/UserScreen/EditProfile.tsx
+++ src/screens/UserScreen/EditProfile.tsx
@@ -65,7 +65,10 @@ const EditProfile: React.FC = () => {
                                         <label htmlFor="file-upload"
                                                className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                                             <span>Upload a file</span>
-                                            <input id="file-upload" name="file-upload" type="file" className="sr-only"/>
+                                            <Field id="file-upload" name="backdropPhoto" type="file"
+                                                   as={"file"}
+                                                   className="sr-only"
+                                            />
                                         </label>
                                         <p className="pl-1">or drag and drop</p>
                                     </div>
